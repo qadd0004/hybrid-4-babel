324425acd8b9ea0625ce8831b2209d8c8dd4b5a8
qadd0004 <qadd0004@algonquinlive.com>
2021-03-26 20:57:01 -0400

    Convert all functions to arrow functions

--- dist/app.js
+++ dist/app.js
@@ -15,21 +15,21 @@ let justAPromise = new Promise((resolve, reject) => {
   console.log(error.message);
 });
 
-async function waitForVictory() {
-  let aPromise = new Promise(function (resolve, reject) {
+let waitForVictory = async () => {
+  let aPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("All is well");
     }, 1000);
   });
   document.querySelector("#async-output").textContent = await aPromise;
-}
+};
 
 let landscape = {
   sky: "blue",
   grass: "Green"
 };
 
-function whichColour(_ref) {
+let whichColour = (_ref) => {
   let {
     sky,
     grass
@@ -37,7 +37,7 @@ function whichColour(_ref) {
   console.log(sky);
   console.log(grass);
   document.querySelector("#destructuring-output").textContent = "The sky is ".concat(sky, " and the grass is ").concat(grass);
-}
+};
 
 whichColour(landscape);
 waitForVictory();
--- js/app.js
+++ js/app.js
@@ -16,27 +16,27 @@ let justAPromise = new Promise((resolve, reject) => {
     console.log(error.message);
   });
 
-async function waitForVictory() {
-  let aPromise = new Promise(function (resolve, reject) {
+let waitForVictory = async () => {
+  let aPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("All is well");
     }, 1000);
   });
   document.querySelector("#async-output").textContent = await aPromise;
-}
+};
 
 let landscape = {
   sky: "blue",
   grass: "Green",
 };
 
-function whichColour({ sky, grass }) {
+let whichColour = ({ sky, grass }) => {
   console.log(sky);
   console.log(grass);
   document.querySelector(
     "#destructuring-output"
   ).textContent = `The sky is ${sky} and the grass is ${grass}`;
-}
+};
 
 whichColour(landscape);
 waitForVictory();
